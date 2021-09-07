294141fb33b95bf1b1a45c576ab214b63977e1f0
alexcibubins <88849851+alexcibubins@users.noreply.github.com>
2021-09-07 15:07:10 +0200

    Update RuthNoRecoil v2.1.py

--- RuthNoRecoil v2.1.py
+++ RuthNoRecoil v2.1.py
@@ -21,32 +21,6 @@ position_top = int(screen_height/2 - window_height/2)
 position_right = int(screen_width/2 - window_width/2)
 root.geometry(f'{window_width}x{window_height}+{position_right}+{position_top}')
 
-"""Key codes and names variables (used below)"""
-f1Name = "f1"
-f1Code = 0x70
-f2Name = "f2"
-f1Code = 0x71
-f3Name = "f3"
-f1Code = 0x72
-f4Name = "f4"
-f1Code = 0x73
-f5Name = "f5"
-f1Code = 0x74
-f6Name = "f6"
-f1Code = 0x75
-f7Name = "f7"
-f1Code = 0x76
-f8Name = "f8"
-f1Code = 0x77
-f9Name = "f9"
-f1Code = 0x78
-f10Name = "f10"
-f1Code = 0x79
-f11Name = "f11"
-f1Code = 0x7A
-f12Name = "f12"
-f1Code = 0x7B
-
 """All the functions below"""
 """Changing the status mode (ON/OFF) when clicking the setted key"""    
 def StatusChange():
@@ -76,20 +50,47 @@ validKeysInfo()
 def newKey():
         key = StartStopKeyEntry.get()
         key = key.strip().lower()
-        if (not key=="f1" and not key=="backspace" and not key=="enter" and not key=="capslock" and not key=="num_lock" and not key=="supr" and not key=="tab" and not key=="ctrl" and not key=="f2" and not key=="f3" and not key=="f4" and not key=="f5" and not key=="f6" and not key=="f7" and not key=="f8" and not key=="f9" and not key=="f10" and not key=="f11" and not key=="f12"):
+        if (not key =="" and not key=="f1" and not key=="backspace" and not key=="enter" and not key=="capslock" and not key=="num_lock" and not key=="supr" and not key=="tab" and not key=="ctrl" and not key=="f2" and not key=="f3" and not key=="f4" and not key=="f5" and not key=="f6" and not key=="f7" and not key=="f8" and not key=="f9" and not key=="f10" and not key=="f11" and not key=="f12"):
                 tkinter.messagebox.showerror('Key error', 'Current key Empty or Invalid Key enter\n\nValid keys entrys: \n\n f1 to f12 \n\n CapsLock \n\n Supr \n\n Num_lock  \n\n Tab \n\n Ctrl \n\n Backspace \n\n Enter ')
         else:
-                CurrentKey.config(text="Current key : "+key.upper())
+                if(key==""):
+                        CurrentKey.config(text="Current key : EMPTY")
+                else:
+                        CurrentKey.config(text="Current key : "+key.upper())
         return key
 
-"""If the setted on/off key is pressed on the newKey() return then change the status mode"""
+"""If the setted on/off key is pressed on the newKey return then change the status mode"""
 def on_press():
+        f1Name = "f1"
+        f1Code = win32api.GetKeyState(0x70)
+        f2Name = "f2"
+        f2Code = win32api.GetKeyState(0x71)
+        f3Name = "f3"
+        f3Code = win32api.GetKeyState(0x72)
+        f4Name = "f4"
+        f4Code = win32api.GetKeyState(0x73)
+        f5Name = "f5"
+        f5Code = win32api.GetKeyState(0x74)
+        f6Name = "f6"
+        f6Code = win32api.GetKeyState(0x75)
+        f7Name = "f7"
+        f7Code = win32api.GetKeyState(0x76)
+        f8Name = "f8"
+        f8Code = win32api.GetKeyState(0x77)
+        f9Name = "f9"
+        f9Code = win32api.GetKeyState(0x78)
+        f10Name = "f10"
+        f10Code = win32api.GetKeyState(0x79)
+        f11Name = "f11"
+        f11Code = win32api.GetKeyState(0x7A)
+        f12Name = "f12"
+        f12Code = win32api.GetKeyState(0x7B)
         
-        f1State = win32api.GetKeyState(f1Code)
-        if (f1State < 0 and f1Name == newKey()):
+        if (f1Code<0 and f1Name == newKey() or f2Code<0 and f2Name == newKey() or f3Code<0 and f3Name == newKey() or f4Code<0 and f4Name == newKey() or f5Code<0 and f5Name == newKey() or f6Code<0 and f6Name == newKey() or f7Code<0 and f7Name == newKey() or f8Code<0 and f8Name == newKey() or f9Code<0 and f9Name == newKey() or f10Code<0 and f10Name == newKey() or f11Code<0 and f11Name == newKey() or f12Code<0 and f12Name == newKey()):
                 StatusChange()
                 time.sleep(0.1)
         root.after(100,on_press)
+        
 root.after(100,on_press)
 
 def saveRecoil():
